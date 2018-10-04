4c77059e6b576a486e6b0fb09a2ed5dcd6f971f4
sihc <tvquocchi@gmail.com>
2018-10-04 20:17:20 +0700

    just get title from dantri.com.vn website

--- getTitleInPage.py
+++ getTitleInPage.py
@@ -1,1 +1,13 @@
-getTitleInPage.py
+import requests
+from htmldom import htmldom
+
+url = 'https://dantri.com.vn/xa-hoi/vu-xe-khach-tong-rao-chan-duong-sat-tai-xe-dang-bi-treo-bang-20180823162346704.htm'
+content = requests.get(url)
+
+# print( content.text )
+dom = htmldom.HtmlDom().createDom(content.text)
+
+find = "h2.sapo"
+elem = dom.find( find )
+
+print( elem.html().encode( 'utf-8').decode('utf-8') )
